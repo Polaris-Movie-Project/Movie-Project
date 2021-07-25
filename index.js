f5fdec746d1104bdccfecc96b9c0695edfd225a3
Calais Galbraith <calaisgalbraith@gmail.com>
2021-07-25 18:15:13 -0500

    added notes and formatting changes - Calais

--- index.js
+++ index.js
@@ -2,27 +2,7 @@
 
 const serverURL = "https://ten-coordinated-spectrum.glitch.me/movies"
 
-    // //grabbing data from glitch (GET request)
-    //     fetch(serverURL)
-    //     .then(response => response.json()) //parsing data
-    //     .then(data => console.log(data));  //console.log data
-
-    // //POST Request (send/add data to server)
-        // const objToSend= {
-        //     user: "Samuel",
-        //     message: "Really enjoyed the movies project!"
-        // };
-        // const options = {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify(objToSend), //convert object into pure json object, puts esc characters in front of ""
-        // };
-        // fetch(serverURL, options)
-        //     .then( response => console.log(response) ) /* review was created successfully */
-        //     .catch( error => console.error(error) ); /* handle errors */
-
+    //Call data & get it
     function AJAX(url, method = "GET", data){
 
         const options = {
@@ -39,38 +19,15 @@ const serverURL = "https://ten-coordinated-spectrum.glitch.me/movies"
             .catch(error => error);
     }
 
-    //update server base
-    //"POST" METHOD
-    // AJAX(serverURL, "POST", {title: "jquery ain't got nothing"})
-    //     .then(function (data){
-    //         console.log(data);
-    //     });
-
-    //grab third entry in data base, don't have to specify GET
-    //use to grab individual item
-    // AJAX(serverURL + "/3" )
-    //     .then(data => console.log(data))
-
-
-    //UPDATE/EDIT existing data (whatever has id of 9 in this case)
-    // "PUT METHOD", need to put in entire object want to change
-    // AJAX(serverURL + "/9", "PUT", {
-    //     name: "Polaris",
-    //     message: "We are ready for the weekend!"
-    // })
-    //     .then(data => console.log(data));
-
-    //UPDATE/EDIT existing data
-    //"PATCH METHOD", edit only what put in
-    // AJAX(serverURL + "/9", "PATCH", {
-    //     message: "We are REALLY ready for the weekend!"
-    // })
-    //     .then(data => console.log(data));
-
-    //REMOVE OBJECT/MOVIE
-    // AJAX(serverURL + "/6", "DELETE")
-    //     .then(data => console.log(data))
 
     //DISPLAY DATA
-    // AJAX(serverURL)
-    //     .then(data => console.log(data))
+    AJAX(serverURL)
+        .then(data => console.log(data))
+
+    //TODO: Function to remove loading message once data is called
+
+    //TODO: Function to add data to html (cards?)
+
+    //TODO: Create form and have it update movie data base
+
+    //TODO: Make mobile responsive?
