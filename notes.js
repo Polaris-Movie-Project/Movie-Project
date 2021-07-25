71b9d9aae215b6d3852da022b26e20c17941583e
Calais Galbraith <calaisgalbraith@gmail.com>
2021-07-24 23:34:33 -0500

    added notes -Calais

---
+++ notes.js
@@ -0,0 +1,33 @@
+// DONE: 1: Get project set up
+    // Find your team member(s) -DONE
+    // Create a repository -DONE
+    // Setup glitch server - DONE
+
+// DONE: 2: Create files for Project
+    // create html file -DONE
+    // create JS file -DONE
+    // link html and JS -DONE
+
+// 3: Test glitch server with calls to server
+    // get all movies request
+    // get a single movie request
+    // post movie request
+    // update movie request
+    // delete movie request
+
+// 4: Add contents to HTML
+    // Add loading message
+
+// 5: Add initial request to get all movies
+    // make get request to get movies
+    // update loading message to disappear when request comes through
+    // display all movies on the html page
+
+// 6: Create form for movies
+    //     update html to contain a form for adding movies
+    // add form inputs such as name, rating, description to the form
+    // add event listener to form submit button
+    // create function to send post request to create movie on server based on form inputs
+    // use movie creation function as call back for event listener on submit button
+    // make sure that the page does not reload when submit button is pressed
+    // use prevent default ^
