70ad00e8a2cff672f7c4cc9079fddc8c0cf0da03
Calais Galbraith <calaisgalbraith@gmail.com>
2021-07-28 09:52:27 -0500

    added modal, styling changes, edit functionality improved

--- index.js
+++ index.js
@@ -1,98 +1,232 @@
 "use strict"
 
-const serverURL = "https://ten-coordinated-spectrum.glitch.me/movies"
+const serverURL = "https://candy-cottony-baroness.glitch.me/movies"
 
-    //Call data & get it
-    function AJAX(url, method = "GET", data){
+// const OMDBurl = "http://www.omdbapi.com/?apikey=[1d3b03a8]&"
+// https://omdbapi.com/?t=scott+pilgrim&apikey=1d3b03a8
 
-        const options = {
-            method: method,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
+// //CALL OMDB Data Base
+// function omdbURL(url, method = "GET", data) {
+//     const options = {
+//         method: method,
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(data),
+//     };
+//     fetch(url, options)
+//         .then(response => response.json()) /* Movie was created successfully */
+//         .catch(error => console.error(error)); /* handle errors */
+//
+// }
+// omdbURL("https://omdbapi.com/?t=scott+pilgrim&apikey=1d3b03a8")
+//     .then(data => console.log(data))
+
+
+//CALL GLITCH DATA BASE
+function AJAX(url, method = "GET", data){
+    const options = {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    };
+    return fetch(url, options)
+        .then(response => response.json()) //parsing data
+        .then(responseData => responseData)
+        .catch(error => error);
+};
+
+//-----------------------------------------
+
+//DISPLAY GLITCH DATA
+AJAX(serverURL)
+    .then(data => {console.log(data); displayMovies(data); hideLoading()})
+
+//-----------------------------------------
+
+//HIDE LOADING ICON WHEN DATA IS LOADED
+function hideLoading(){
+    $(".loading").addClass("canSee");
+};
+
+//-----------------------------------------
+
+//ADD MOVIE DATA TO HTML
+function displayMovies(movies) {
+    //resets html to blank, so when user adds movie page is reset
+    $("#movieContainer").html("");
+
+    //generates html for displaying movie
+    movies.forEach(function (movie) {
+        $("#movieContainer").append(`<div class="card col-md-4 mb-4 bg-light ">
+                                    <img class="card-img-top" src="${movie.poster}" alt="Example Image">
+                                    <div class="card-body">
+                                        <h4 class="card-title overflow-auto" id="localMovie${movie.id}">${movie.title}</h4>
+                                        <p class="card-text overflow-auto" id="movieYear${movie.id}">${movie.year}</p>
+                                        <p class="card-text overflow-auto" id="movieRating${movie.id}">${movie.rating}</p>
+                                        <p class="card-text overflow-auto" id="moviePlot${movie.id}">${movie.plot}</p>
+                                    </div>
+                                    <div class="btn-group"></div>
+                                         <button type="button" id="editButton${movie.id}" class="editButton" data-id=${movie.id}>Edit</button>
+                                        <button type="button" id="deleteButton${movie.id}" class="deleteButton" data-id=${movie.id}>Delete</button>
+                                     </div>
+                                     </div>`);
+    });
+};
+
+//-----------------------------------------
+
+//DELETE MOVIE WHEN CLICKED
+$(document).on("click",".deleteButton",function() {
+    const actuallyDelete = confirm("Do you really want to delete selected movie?");
+    if(actuallyDelete){
+        AJAX(serverURL + "/" + $(this).attr("data-id"), "DELETE")
+            .then(AJAX(serverURL)
+                .then(data => displayMovies(data)))
+    }
+});
+
+//-----------------------------------------
+
+//SORT BY SELECTED GENRE
+
+//when new genre is selected, call updateGenreMovie and give it data + selected genre value
+$("#genreSelect").change(function (event){
+    event.preventDefault();
+    AJAX(serverURL)
+        .then(data => updateGenreMovie(data, $("#genreSelect").val()));
+});
+
+//function to only display movies that match selected genre
+function updateGenreMovie(movies, genre) {
+    //resets html to blank, so when user adds movie page is reset
+    $("#movieContainer").html("");
+
+    //generates html for displaying movie (only if match selected genre)
+    movies.forEach(function (movie) {
+        if(movie.genre.includes(genre)){
+            $("#movieContainer").append(`<div class="card col-md-4 mb-4 bg-light ">
+                                            <img class="card-img-top" src="${movie.poster}" alt="Example Image">
+                                            <div class="card-body">
+                                                <h4 class="card-title overflow-auto" id="localMovie${movie.id}">${movie.title}</h4>
+                                                <p class="card-text overflow-auto" id="movieYear${movie.id}">${movie.year}</p>
+                                                <p class="card-text overflow-auto" id="movieRating${movie.id}">${movie.rating}</p>
+                                                <p class="card-text overflow-auto" id="moviePlot${movie.id}">${movie.plot}</p>
+                                            </div>
+                                            <div class="btn-group"></div>
+                                                 <button type="button" id="editButton${movie.id}" class="editButton" data-id=${movie.id}>Edit</button>
+                                                <button type="button" id="deleteButton${movie.id}" class="deleteButton" data-id=${movie.id}>Delete</button>
+                                             </div>
+                                             </div>`);
+        }
+        else if(genre === "Genre"){
+            displayMovies(movies);
+        }
+    });
+};
+
+//-----------------------------------------
+
+// SORT BY SELECTED RATING
+
+//when new rating is selected, call updateRatingMovie and give it data + selected rating value
+$("#ratingSelect").change(function (event){
+    event.preventDefault();
+    AJAX(serverURL)
+        .then(data => updateRatingMovie(data, $("#ratingSelect").val()));
+});
+
+//function to only display movies that match selected rating
+function updateRatingMovie(movies, rating) {
+    //resets html to blank, so when user adds movie page is reset
+    $("#movieContainer").html("");
+
+    //generates html for displaying movie (only if match selected genre)
+    movies.forEach(function (movie) {
+        if(movie.rating.includes(rating)){
+            $("#movieContainer").append(`<div class="card col-md-4 mb-4 bg-light ">
+                                        <img class="card-img-top" src="${movie.poster}" alt="Example Image">
+                                        <div class="card-body">
+                                            <h4 class="card-title overflow-auto" id="localMovie${movie.id}">${movie.title}</h4>
+                                            <p class="card-text overflow-auto" id="movieYear${movie.id}">${movie.year}</p>
+                                            <p class="card-text overflow-auto" id="movieRating${movie.id}">${movie.rating}</p>
+                                            <p class="card-text overflow-auto" id="moviePlot${movie.id}">${movie.plot}</p>
+                                        </div>
+                                        <div class="btn-group"></div>
+                                             <button type="button" id="editButton${movie.id}" class="editButton" data-id=${movie.id}>Edit</button>
+                                            <button type="button" id="deleteButton${movie.id}" class="deleteButton" data-id=${movie.id}>Delete</button>
+                                         </div>
+                                         </div>`);
+        }
+        else if(rating === "Rating"){
+            displayMovies(movies);
+        }
+    });
+};
+
+//-----------------------------------------
+
+//EDIT WITH MODAL
+    // Get the modal
+        var modal = document.getElementById("myModal");
+
+    // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+    //When close button is clicked, closes model
+        span.onclick = function() {
+            modal.style.display = "none";
         };
 
-        return fetch(url, options)
-            .then(response => response.json()) //parsing data
-            .then(responseData => responseData)
-            .catch(error => error);
-    }
+    //function to close modal
+        function closeModal(){
+            modal.style.display = "none";
+        }
 
-    //DISPLAY DATA
-    AJAX(serverURL)
-        .then(data => {console.log("Inital Data Load:"); console.log(data); displayMovies(data); hideLoading()})
-
-    //Hide loading data when data is loaded
-    function hideLoading(){
-        $(".loading").addClass("canSee");
-    };
-
-    //Add movie data to HTML
-    function displayMovies(movies) {
-        //resets html to blank, so when user adds movie page is reset
-        $("#movieContainer").html("");
-
-        //generates html for displaying movie
-        movies.forEach(function (movie) {
-            $("#movieContainer").append(`<div class="col">
-                                <h4 id="localMovie${movie.title}">${movie.title}</h4>
-                                <img src="${movie.poster}" alt="Example Image">
-                                 <p id="movieYear${movie.year}">${movie.year}</p>
-                                 <p id="movieRating${movie.rating}">${movie.rating}</p>
-                                 <p id="moviePlot${movie.plot}">${movie.plot}</p>
-                                 <button type="button" id="editButton${movie.id}" class="editButton" data-id=${movie.id}>Edit</button>
-                                 <button type="button" id="deleteButton${movie.id}" class="deleteButton" data-id=${movie.id}>Delete</button>
-                                 </div>`);
-        });
-    }
-
-    //DELETES MOVIE WHEN CLICKED
-    $(document).on("click",".deleteButton",function() {
-      const actuallyDelete = confirm("Do you really want to delete selected movie?");
-      if(actuallyDelete){
-          AJAX(serverURL + "/" + $(this).attr("data-id"), "DELETE")
-              .then(AJAX(serverURL)
-                  .then(data => displayMovies(data)))
-      }
-    });
-
-
-//one modal in index and all edit buttons bring it up
-
-//closes form when "closed" is clicked/selected
-    function closeForm() {
-        document.getElementById("myForm").style.display = "none";
-    }
-
-//when edit button clicked, pulls up form for input
-    $(document).on("click",".editButton",function() {
-
-            //sets number for ID to reference to later
-            let testID = $(this).attr("data-id").val();
-
-            console.log($("(#localMovie + testID)"))
-
-            let movieObject = {
-                title: "test",
-                year: "test",
-                rating: "test",
-                plot: "test"
+    // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
             }
+        };
+
+    //default value for testID
+        let testID ="";
+
+    // When the user clicks the edit button, opens the modal
+        $(document).on("click",".editButton",function(){
+
+            //displays modal
+            modal.style.display = "block";
+
+            // sets number for ID to reference to later
+            testID = $(this).attr("data-id");
+            console.log(testID);
+            console.log($("#localMovie" + testID).text());
+
+            //DONE: set current values of year/rating/plot as default form text
+            let movieObject = {
+                title: $("#localMovie" + testID).text(),
+                year: $("#movieYear" + testID).text(),
+                rating: $("#movieRating" + testID).text(),
+                plot: $("#moviePlot" + testID).text()
+            }
+
             //makes default text current movie properties
             $("#movieTitle").val(movieObject.title);
             $("#yearReleased").val(movieObject.year);
             $("#movieRating").val(movieObject.rating);
             $("#moviePlot").val(movieObject.plot);
-           //TODO: set current values of year/rating/plot as default form text
 
-            //displays pop-up form on click
-            $("#myForm").css("display", "block");
+        });
 
-        //upon submit click, edit current movie values to whatever input value is
+    // upon submit click, edit current movie values to whatever input value is
         $('#editSubmit').click(function (event){
             event.preventDefault();
             console.log("Submit clicked!");
+            console.log(testID);
 
             //PATCH
             AJAX(serverURL + "/" + testID, "PATCH", {
@@ -101,91 +235,5 @@ const serverURL = "https://ten-coordinated-spectrum.glitch.me/movies"
                 rating: $("#movieRating").val(),
                 plot: $("#moviePlot").val()
             }).then(AJAX(serverURL)
-                .then(data => {displayMovies(data); closeForm()}))
+                .then(data => {displayMovies(data); closeModal()}))
         });
-    });
-
-//upon click of submit button, updates data and regenerates movies
-    $('#submit').click(function(event) {
-        event.preventDefault();
-
-        //POST - Update data with new user input from form
-        AJAX(serverURL, "POST",
-            {title: $("#title").val(),
-            year: $("#year").val(),
-            rating: $("#rating").val(),
-            plot: $("#plot").val()
-            })
-            .then(function (data){
-                console.log(data);
-            });
-
-        //Refreshes movie data to display user input/movie
-        AJAX(serverURL)
-            .then(data => {console.log("Data load after user input:"); console.log(data); displayMovies(data); hideLoading()})
-    });
-
-
-//sort by genres when new genre is selected
-    $("#genreSelect").change(function (event){
-        event.preventDefault();
-
-        AJAX(serverURL)
-            .then(data => updateGenreMovie(data, $("#genreSelect").val()));
-    });
-
-//function to only display movies that match selected genre
-    function updateGenreMovie(movies, genre) {
-        //resets html to blank, so when user adds movie page is reset
-        $("#movieContainer").html("");
-
-        //generates html for displaying movie (only if match selected genre)
-        movies.forEach(function (movie) {
-            if(movie.genre.includes(genre)){
-                $("#movieContainer").append(`<div class="col">
-                                    <h4>${movie.title}</h4>
-                                    <img src="${movie.poster}" alt="Example Image">
-                                     <p id="movieYear${movie.year}">${movie.year}</p>
-                                     <p id="movieRating${movie.rating}">${movie.rating}</p>
-                                     <p id="moviePlot${movie.plot}">${movie.plot}</p>
-                                     <button type="button" id="editButton${movie.id}" class="editButton" data-id=${movie.id}>Edit</button>
-                                     <button type="button" id="deleteButton${movie.id}" class="deleteButton" data-id=${movie.id}>Delete</button>
-                                     </div>`);
-            }
-            else if(genre === "Genre"){
-                displayMovies(movies);
-            }
-        });
-    }
-
-//sort by rating when new rating is selected
-    $("#ratingSelect").change(function (event){
-        event.preventDefault();
-        AJAX(serverURL)
-            .then(data => updateRatingMovie(data, $("#ratingSelect").val()));
-});
-
-
-//function to only display movies that match selected rating
-    function updateRatingMovie(movies, rating) {
-        //resets html to blank, so when user adds movie page is reset
-        $("#movieContainer").html("");
-
-        //generates html for displaying movie (only if match selected genre)
-        movies.forEach(function (movie) {
-            if(movie.rating.includes(rating)){
-                $("#movieContainer").append(`<div class="col">
-                                        <h4>${movie.title}</h4>
-                                        <img src="${movie.poster}" alt="Example Image">
-                                         <p id="movieYear${movie.year}">${movie.year}</p>
-                                         <p id="movieRating${movie.rating}">${movie.rating}</p>
-                                         <p id="moviePlot${movie.plot}">${movie.plot}</p>
-                                         <button type="button" id="editButton${movie.id}" class="editButton" data-id=${movie.id}>Edit</button>
-                                         <button type="button" id="deleteButton${movie.id}" class="deleteButton" data-id=${movie.id}>Delete</button>
-                                         </div>`);
-            }
-            else if(rating === "Rating (Stars)"){
-                displayMovies(movies);
-            }
-        });
-    }
